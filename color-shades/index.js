95888e85682db0e240a37192d8d58d4baa5c1140
Marko Krstic <mailme@markokrstic.com>
2023-05-17 16:20:50 +0200

    ...

--- color-shades/index.js
+++ color-shades/index.js
@@ -1,88 +1,107 @@
+// Cache DOM elements
 var colorPicker = document.getElementById("colorPicker");
 var shadesContainer = document.getElementById("shadesContainer");
+var addButton = document.getElementById("addButton");
+var removeButton = document.getElementById("removeButton");
+var hueInput = document.getElementById("hue");
+var colorNameInput = document.querySelector('input[type="text"]');
+var previewCode = document.querySelector('#preview code');
+var debounceTimer = null;
+var hueDebounceTimer = null;
 var shades = [];
 var selectedIndex = null;
 var numBlocks = 10; // Initial number of blocks
 
+// Event listener for colorPicker change
 colorPicker.addEventListener("change", function() {
   generateShades();
   setColorName();
   exportColors();
 });
 
-generateShades(); // Generate shades when the page loads
-setColorName(); // Set the color name when the page loads
-
-function generateShades() {
-    shadesContainer.innerHTML = "";
-    var baseColor = chroma(colorPicker.value);
-    
-    // Adjust the color scale to always start from white and end at black
-    shades = chroma.scale(['white', baseColor, 'black']).mode("lab").colors(numBlocks);
-  
-    shades.forEach(function (shade, index) {
-      var shadeElement = document.createElement("div");
-      shadeElement.style.backgroundColor = shade;
-      shadeElement.className = "shade";
-      shadeElement.textContent = chroma(shade).hex(); // Set the hex value as text content
-  
-      shadeElement.addEventListener("click", function () {
-        setSelectedShade(index);
-      });
-  
-      shadesContainer.appendChild(shadeElement);
-    });
-  
-    // Reapply the selected shade if one is currently selected
-    if (selectedIndex !== null) {
-      setSelectedShade(selectedIndex);
-    }
-}
-
-
-function setSelectedShade(index) {
-    var selectedColor = chroma(colorPicker.value).hex();
-  
-    // Remove the style from the previously selected shade
-    if (selectedIndex !== null) {
-      shadesContainer.childNodes[selectedIndex].classList.remove("active-color");
-    }
-  
-    // Set the style for the selected shade
-    shadesContainer.childNodes[index].style.backgroundColor = selectedColor;
-    shadesContainer.childNodes[index].classList.add("active-color");
-  
-    for (var i = 0; i < shadesContainer.childNodes.length; i++) {
-      var shade;
-      if (i < index) {
-        shade = chroma(selectedColor).brighten((index - i) / 2).hex(); // Reverse darken and brighten methods
-      } else if (i > index) {
-        shade = chroma(selectedColor).darken((i - index) / 2).hex(); // Reverse darken and brighten methods
-      } else {
-        shade = selectedColor;
-      }
-      shadesContainer.childNodes[i].style.backgroundColor = shade;
-      shadesContainer.childNodes[i].textContent = shade; // Update the box name (hex color value)
-    }
-  
-    // Update the selectedIndex
-    selectedIndex = index;
-  
-    exportColors();
-  }
-  
-
-var addButton = document.getElementById("addButton");
-var removeButton = document.getElementById("removeButton");
-
+// Event listener for addButton click
 addButton.addEventListener("click", function() {
   addBlock();
   exportColors();
 });
+
+// Event listener for removeButton click
 removeButton.addEventListener("click", function() {
   removeBlock();
   exportColors();
 });
+
+// Event listener for colorNameInput input with debounce
+colorNameInput.addEventListener('input', function() {
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(exportColors, 500); // Adjust the debounce delay as needed
+});
+
+// Event listener for hueInput input with debounce
+hueInput.addEventListener('input', function() {
+  clearTimeout(hueDebounceTimer);
+  hueDebounceTimer = setTimeout(function() {
+    setColorWithHue();
+    generateShades();
+    setColorName();
+    exportColors();
+  }, 200); // Adjust the debounce delay as needed
+});
+
+// Generate shades when the page loads
+generateShades();
+setColorName();
+exportColors();
+
+function generateShades() {
+  shadesContainer.innerHTML = "";
+  var baseColor = chroma(colorPicker.value);
+  shades = chroma.scale(['white', baseColor, 'black']).mode("lab").colors(numBlocks);
+  
+  shades.forEach(function (shade, index) {
+    var shadeElement = document.createElement("div");
+    shadeElement.style.backgroundColor = shade;
+    shadeElement.className = "shade";
+    shadeElement.textContent = chroma(shade).hex();
+  
+    shadeElement.addEventListener("click", function () {
+      setSelectedShade(index);
+    });
+  
+    shadesContainer.appendChild(shadeElement);
+  });
+  
+  if (selectedIndex !== null) {
+    setSelectedShade(selectedIndex);
+  }
+}
+
+function setSelectedShade(index) {
+  var selectedColor = chroma(colorPicker.value).hex();
+  
+  if (selectedIndex !== null) {
+    shadesContainer.childNodes[selectedIndex].classList.remove("active-color");
+  }
+  
+  shadesContainer.childNodes[index].style.backgroundColor = selectedColor;
+  shadesContainer.childNodes[index].classList.add("active-color");
+  
+  for (var i = 0; i < shadesContainer.childNodes.length; i++) {
+    var shade;
+    if (i < index) {
+      shade = chroma(selectedColor).brighten((index - i) / 2).hex();
+    } else if (i > index) {
+      shade = chroma(selectedColor).darken((i - index) / 2).hex();
+    } else {
+      shade = selectedColor;
+    }
+    shadesContainer.childNodes[i].style.backgroundColor = shade;
+    shadesContainer.childNodes[i].textContent = shade;
+  }
+  
+  selectedIndex = index;
+  exportColors();
+}
 
 function addBlock() {
   numBlocks++;
@@ -90,24 +109,23 @@ function addBlock() {
 }
 
 function removeBlock() {
-    if (numBlocks > 1) {
-      numBlocks--;
-      if (selectedIndex !== null && selectedIndex >= numBlocks) {
-        // If the selected index is out of range after removing a block,
-        // update it to the last available index
-        selectedIndex = numBlocks - 1;
-      }
-      generateShades();
+  if (numBlocks > 1) {
+    numBlocks--;
+    if (selectedIndex !== null && selectedIndex >= numBlocks) {
+      selectedIndex = numBlocks - 1;
     }
+    generateShades();
+    exportColors();
   }
-  
-  function setColorName() {
+}
+
+function setColorName() {
     var colorName = chroma(colorPicker.value).name();
-    document.querySelector('input[type="text"]').value = colorName;
+    colorNameInput.value = colorName;
   }
   
   function exportColors() {
-    var colorName = document.querySelector('input[type="text"]').value;
+    var colorName = colorNameInput.value;
     var output = "colors: {\n";
     output += "  '" + colorName + "': {\n";
   
@@ -115,35 +133,20 @@ function removeBlock() {
       output += "    " + ((index + 1) * 100) + ": '" + chroma(shade).hex() + "',\n";
     });
   
-    output += "  },\n},";
-    
-    document.querySelector('#preview code').textContent = output;
+    output += "  },\n}";
+  
+    previewCode.textContent = output;
   }
   
-  document.querySelector('input[type="text"]').addEventListener('input', exportColors);
+  function setColorWithHue() {
+    var hueValue = hueInput.value;
+    var baseColor = chroma(colorPicker.value);
+    var colorWithHue = baseColor.set('hsl.h', hueValue);
+  
+    colorPicker.value = colorWithHue.hex();
+  }
   
   // Set the color name to "custom-color" when the page loads
-  document.querySelector('input[type="text"]').value = "custom-color";
+  colorNameInput.value = "custom-color";
   exportColors();
   
-
-
-var hueInput = document.getElementById("hue");
-
-hueInput.addEventListener("input", function() {
-  setColorWithHue();
-  generateShades();
-  setColorName();
-  exportColors();
-});
-
-
-
-function setColorWithHue() {
-  var hueValue = hueInput.value;
-  var baseColor = chroma(colorPicker.value);
-  var colorWithHue = baseColor.set('hsl.h', hueValue);
-
-  // Set the color picker's value to the new color
-  colorPicker.value = colorWithHue.hex();
-}
