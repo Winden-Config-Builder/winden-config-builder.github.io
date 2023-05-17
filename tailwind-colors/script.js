1ff585a9a97080a4498712d53f6c77d1f2fdd18d
Marko Krstic <mailme@markokrstic.com>
2023-05-17 19:05:49 +0200

    ...

--- tailwind-colors/script.js
+++ tailwind-colors/script.js
@@ -1,21 +1,52 @@
 window.onload = generatePalette;
 
+let numBlocks = 5;
+
+document.getElementById('addButton').onclick = function() {
+    if (numBlocks < 10) {
+        numBlocks++;
+        generatePalette();
+    }
+};
+
+document.getElementById('removeButton').onclick = function() {
+    if (numBlocks > 1) {
+        numBlocks--;
+        generatePalette();
+    }
+};
+
+document.getElementById('colorPicker').onchange = function() {
+    document.getElementById('colorPickerValue').value = this.value;
+    generatePalette();
+};
+
+document.getElementById('colorPickerValue').onchange = function() {
+    document.getElementById('colorPicker').value = this.value;
+    generatePalette();
+};
+
 function generatePalette() {
     let pickedColor = document.getElementById('colorPicker').value;
     let paletteDiv = document.getElementById('palette');
-    
+
     // clear existing palette
     paletteDiv.innerHTML = '';
 
     // create shades from white to picked color
-    for (let i = 0; i <= 4; i++) {
-        let color = color2k.mix('white', pickedColor, i * 0.2);
+    for (let i = 0; i < numBlocks - 1; i++) {
+        let ratio = i / (numBlocks - 1);
+        let color = color2k.mix('white', pickedColor, ratio);
         createColorDiv(color);
     }
 
+    // create the picked color block
+    // createColorDiv(pickedColor);
+
     // create shades from picked color to black
-    for (let i = 1; i <= 4; i++) {
-        let color = color2k.mix(pickedColor, 'black', i * 0.2);
+    for (let i = 1; i < numBlocks; i++) {
+        let ratio = i / (numBlocks - 1);
+        let color = color2k.mix(pickedColor, 'black', ratio);
         createColorDiv(color);
     }
 }
