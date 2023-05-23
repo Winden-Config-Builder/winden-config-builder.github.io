ac30f79d82e31fff43673e5148bf5b7a348ef951
Marko Krstic <mailme@markokrstic.com>
2023-05-23 14:34:44 +0200

    ... react

---
+++ my-react-app/src/CodePreview.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const CodePreview = () => {
+  return (
+    <div>
+      <div id="css-variables-preview" className="code-preview">
+        <pre>
+          <code></code>
+        </pre>
+      </div>
+      <div id="tailwind-var-preview" className="code-preview">
+        <pre>
+          <code></code>
+        </pre>
+      </div>
+      <div id="tailwind-preview" className="code-preview">
+        <pre>
+          <code></code>
+        </pre>
+      </div>
+    </div>
+  );
+};
+
+export default CodePreview;
---
+++ my-react-app/src/FontPreview.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FontPreview = () => {
+  return (
+    <div className="font-preview">
+      <p className="sm">Lorem ipsum</p>
+      <p className="md">Lorem ipsum</p>
+      <p className="base">Lorem ipsum</p>
+      <p className="lg">Lorem ipsum</p>
+      <p className="xl">Lorem ipsum</p>
+      <p className="xxl">Lorem ipsum</p>
+    </div>
+  );
+};
+
+export default FontPreview;
