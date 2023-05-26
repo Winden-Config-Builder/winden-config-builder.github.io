1b24711ae632d6f24ccb738fc95ded4eb3c8b603
Marko Krstic <mailme@markokrstic.com>
2023-05-26 11:17:26 +0200

    ...

--- builder/src/js/includeHTML.js
+++ builder/src/js/includeHTML.js
@@ -1,31 +1,22 @@
-export function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain attribute:*/
-    file = elmnt.getAttribute("dp-include-html");
+export async function includeHTML() {
+  const elements = document.querySelectorAll('[dp-include-html]');
+  
+  let promises = Array.from(elements).map(async elmnt => {
+    let file = elmnt.getAttribute('dp-include-html');
+    
     if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {
-            elmnt.innerHTML = this.responseText;
-          }
-          if (this.status == 404) {
-            elmnt.innerHTML = "Page not found.";
-          }
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("dp-include-html");
-          includeHTML();
-        }
-      };
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
+      try {
+        let response = await fetch(file);
+        if (!response.ok) throw new Error("404");
+        let text = await response.text();
+        elmnt.innerHTML = text;
+      } catch (e) {
+        elmnt.innerHTML = "Page not found.";
+      } finally {
+        elmnt.removeAttribute('dp-include-html');
+      }
     }
-  }
+  });
+  
+  await Promise.all(promises);
 }
--- builder/tailwind.config.js
+++ builder/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.html"],
+  content: ["./src/**/*.{html,js}", "./*.html", "./inc/*.html"],
   theme: {
     extend: {},
   },
