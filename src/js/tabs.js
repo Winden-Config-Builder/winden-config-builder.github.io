d3e1216b0ab923adef2792c8c1fcc41df5cf0327
Marko Krstic <mailme@markokrstic.com>
2023-06-02 13:04:19 +0200

    Update tabs.js

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,6 +1,6 @@
 // Get the first tab button and simulate a click on page load
 window.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('colorsTab').click();
+    document.getElementById('breakpointsTab').click();
   });
 
   function openTab(evt, tabName) {
